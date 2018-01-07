9feffad21035be6e7ee469779405dc72a06f47b3
benhurdavies <benhurdavies@gmail.com>
2018-01-07 12:38:29 +0530

    initial commit

---
+++ FlatingArray/FlatArray.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlatingArray
+{
+    public class FlatArray
+    {
+        public FlatArray(object input)
+        {
+            _FlatArray = new LinkedList<int>();
+            _input = input;
+        }
+
+        object _input = null;
+        LinkedList<int> _FlatArray = null;
+
+        public int[] makeFlating(){
+            Process(_input);
+            return _FlatArray.ToArray();
+        }
+        private void Process(object input)
+        {
+            if (validate(input))
+            {
+                if (input.GetType().IsArray)
+                {
+                    foreach (var item in (Array)input)
+                    {
+                        Process(item);
+                    }
+                }
+                else
+                {
+                    _FlatArray.AddLast((int)input);
+                }
+            }
+            else
+            {
+                throw new Exception("Not an expected object type");
+            }
+        }
+
+        private bool validate(object input){
+            var k = input.GetType();
+            if (input.GetType().IsArray || input.GetType() == typeof(int))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ FlatingArrayTest/FlatingArrayTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlatingArray;
+
+namespace FlatingArrayTest
+{
+    [TestClass]
+    public class FlatingArrayTest
+    {
+        [TestMethod]
+        public void GivenCase()
+        {
+            object input = new object[] { new object[] { 1, 2, new int[] { 3 } }, 4 };
+            int[] expectedOutput = new int[] {1,2,3,4 };
+            int[] output = (new FlatingArray.FlatArray(input)).makeFlating();
+            CollectionAssert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod]
+        public void TestComplexObject()
+        {
+            object input = new object[] { 5, new int[] { 2, 8 }, new object[] { 1, 2, new int[] { 7, 8 } }, 9 };
+            int[] expectedOutput = new int[] { 5, 2, 8, 1, 2, 7, 8, 9 };
+            int[] output =(new FlatingArray.FlatArray(input)).makeFlating();
+            CollectionAssert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod]
+        public void TestEmptyObject()
+        {
+            object input = new object[] { };
+            int[] expectedOutput = new int[] { };
+            int[] output = (new FlatingArray.FlatArray(input)).makeFlating();
+            CollectionAssert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Not an expected object type")]
+        public void TestFailedCase()
+        {
+            object input = new object[] { 5, new int[] { 2, 8 }, new object[] { 1, 2, new int[] { 7, 8 } }, "5" };
+            int[] output = (new FlatingArray.FlatArray(input)).makeFlating();
+        }
+    }
+}
